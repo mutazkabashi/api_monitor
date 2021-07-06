b90ae63b9bc68b37352bc0b6d420e2ce008de1d1
mutazhkabashi <mutazhkabbashi@gmail.com>
2021-07-06 06:03:04 +0300

    fix bug in mainPage.js

--- src/main/app/src/components/MainPage.js
+++ src/main/app/src/components/MainPage.js
@@ -31,7 +31,7 @@ export default function MainPage() {
 
         <Switch>
           <Route exact path="/">
-            <Dashboard />
+            <ProfileView />
           </Route>
 
           <Route exact path="/dashboard">
